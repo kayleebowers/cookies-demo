ae52c772f6e53d0f81aea89662090b12d18c9995
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-11 17:20:34 +0000

    feat: create cookies handler middleware

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const Cookies = require("cookies");
 
 const app = express();
+let cookies = null;
+
+// initialize the cookies handler by creating middleware and forward req/res to the next one
+app.use((req, res, next) => {
+    cookies = new Cookies(req, res)
+    cookies.set("name", "Sabrina", {expires: new Date("December 17, 2023 03:24:00")});
+    next();
+})
 
 app.get("/", (req, res) => {
     res.send("Hello test");
